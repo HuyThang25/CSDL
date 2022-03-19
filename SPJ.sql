4dd651a793e57fb9266d63250e057dd51af207be
HuyThang25 <93728466+HuyThang25@users.noreply.github.com>
2022-03-19 20:53:58 +0700

    Create SPJ.sql

---
+++ SPJ.sql
@@ -0,0 +1,75 @@
+﻿--     DDL
+CREATE DATABASE SPJ
+USE SPJ
+-- Tao bang
+CREATE TABLE NCC 
+(
+	MaNCC char(5) not null,
+	Ten varchar(40) not null,
+	Heso int check (Heso>=0 and Heso<=100) default  0,
+	ThPho varchar(20) not null
+)
+CREATE TABLE VATTU
+(
+	MaVT char(5) not null,
+	Ten varchar(40) not null,
+	Mau varchar(15) unique,
+	TrLuong float check (TrLuong>2.0)
+)
+CREATE TABLE DUAN
+(
+	MaDA char(5) not null,
+	Ten varchar(40) not null,
+	ThPho varchar(20) not null
+)
+CREATE TABLE CC
+(
+	MaNCC char(5) not null,
+	MaVT char(5) not null,
+	MaDA char(5) not null,
+	SoLuong int default 0
+)
+DROP TABLE NCC
+DROP TABLE VATTU
+DROP TABLE DUAN
+DROP TABLE CC
+-- Rang buoc khoa chinh
+ALTER TABLE NCC
+	ADD CONSTRAINT PK_NCC
+	PRIMARY KEY (MaNCC)
+ALTER TABLE VATTU
+	ADD CONSTRAINT PK_VATTU
+	PRIMARY KEY (MaVT)
+
+ALTER TABLE DUAN
+	ADD CONSTRAINT PK_DUAN
+	PRIMARY KEY (MaDA)
+ALTER TABLE CC
+	ADD CONSTRAINT PK_CC
+	PRIMARY KEY (MaNCC, MaVT, MaDA)
+-- Rang buoc khoa ngoai
+ALTER TABLE CC
+	ADD CONSTRAINT FK_CC_ref_NCC
+	FOREIGN KEY (MaNCC)
+	REFERENCES NCC(MaNCC)
+ALTER TABLE CC
+	ADD CONSTRAINT FK_CC_ref_VATTU
+	FOREIGN KEY (MaVT)
+	REFERENCES VATTU(MaVT)
+ALTER TABLE CC
+	ADD CONSTRAINT FK_CC_ref_DUAN
+	FOREIGN KEY (MaDA)
+	REFERENCES DUAN(MaDA)
+-- Tao index
+CREATE INDEX ID_NCC ON NCC(MaNCC)
+CREATE INDEX ID_VATTU ON VATTU(MaVT)
+CREATE INDEX ID_DUAN ON DUAN(MaDA)
+CREATE INDEX ID_CC ON CC(MaNCC, MaVT, MaDA)
+
+ALTER TABLE VATTU
+	ADD ThPho varchar(15) not null default 'Ha Noi'
+ALTER TABLE DUAN
+	ADD DiaDiem nText default 'Hà Nội'
+SELECT * FROM DUAN
+
+--DML
