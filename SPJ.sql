addc34661c1954a5586a6739e9d9de6532d7680f
HuyThang25 <93728466+HuyThang25@users.noreply.github.com>
2022-03-20 17:20:23 +0700

    Update SPJ.sql

--- SPJ.sql
+++ SPJ.sql
@@ -13,7 +13,7 @@ CREATE TABLE VATTU
 (
 	MaVT char(5) not null,
 	Ten varchar(40) not null,
-	Mau varchar(15) unique,
+	Mau varchar(15) not null,
 	TrLuong float check (TrLuong>2.0)
 )
 CREATE TABLE DUAN
@@ -40,7 +40,6 @@ ALTER TABLE NCC
 ALTER TABLE VATTU
 	ADD CONSTRAINT PK_VATTU
 	PRIMARY KEY (MaVT)
-
 ALTER TABLE DUAN
 	ADD CONSTRAINT PK_DUAN
 	PRIMARY KEY (MaDA)
@@ -67,9 +66,54 @@ CREATE INDEX ID_DUAN ON DUAN(MaDA)
 CREATE INDEX ID_CC ON CC(MaNCC, MaVT, MaDA)
 
 ALTER TABLE VATTU
-	ADD ThPho varchar(15) not null default 'Ha Noi'
-ALTER TABLE DUAN
-	ADD DiaDiem nText default 'Hà Nội'
-SELECT * FROM DUAN
+	ADD ThPho varchar(20) not null
 
 --DML
+INSERT INTO NCC VALUES
+	('S1','Son',20,'TpHCM'),
+	('S2','Tran',10,'Ha Noi'),
+	('S3','Bach',30,'Ha Noi'),
+	('S4','Lap',20,'TpHCM'),
+	('S5','Anh',30,'Da Nang')
+SELECT * FROM NCC
+
+INSERT INTO DUAN VALUES
+	('J1','May phan loai','Ha Noi'),
+	('J2','Man hinh','Viet Tri'),
+	('J3','OCR','Da Nang'),
+	('J4','Bang dieu khien','Da Nang'),
+	('J5','RAID','TpHCM'),
+	('J6','EDS','Hai Phong'),
+	('J7','Bang tu','TpHCM')
+INSERT INTO VATTU VALUES
+	('P1','Dai oc','Do',12.0,'TpHCM'),
+	('P2','Bu long','Xanh la',17.0,'Ha Noi'),
+	('P3','Dinh vit','Xanh Duong',17.0,'Hai Phong'),
+	('P4','Dinh vit','Do',14.0,'TpHCM'),
+	('P5','Cam','Xanh duong',12.0,'Ha Noi'),
+	('P6','Bang rang','do',19.0,'TpHCM')
+INSERT INTO CC VALUES 
+	('S1','P1','J1',200),
+	('S1','P1','J4',700),
+	('S2','P3','J1',400),
+	('S2','P3','J2',200),
+	('S2','P3','J3',200),
+	('S2','P3','J4',500),
+	('S2','P3','J5',600),
+	('S2','P3','J6',400),
+	('S2','P3','J7',800),
+	('S2','P5','J2',100),
+	('S2','P3','J1',200),
+	('S3','P4','J2',500),
+	('S3','P6','J3',300),
+	('S4','P6','J7',300),
+	('S4','P2','J2',200),
+	('S5','P2','J4',100),
+	('S5','P5','J5',500),
+	('S5','P5','J7',100),
+	('S5','P6','J2',200),
+	('S5','P1','J4',100),
+	('S5','P3','J4',200),
+	('S5','P4','J4',800),
+	('S5','P5','J4',400),
+	('S5','P6','J4',500)
